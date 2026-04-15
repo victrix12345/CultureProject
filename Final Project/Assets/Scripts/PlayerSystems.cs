5f5d858fb21a03e591cbd168650fab2f66805258
victrix12345 <samborski09@gmail.com>
2026-04-15 20:20:58 +0100

    map base

---
+++ Final Project/Assets/Scripts/PlayerSystems.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerSystems : MonoBehaviour
+{
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
