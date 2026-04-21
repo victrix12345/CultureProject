95c2965d1c0173fceb6c48183d1730c26ec86c77
victrix12345 <samborski09@gmail.com>
2026-04-21 17:00:31 +0100

    Update PlayerSystems.cs

--- Final Project/Assets/Scripts/PlayerSystems.cs
+++ Final Project/Assets/Scripts/PlayerSystems.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class PlayerSystems : MonoBehaviour
 {
+    private InputSystem_Actions inputActions;
+
     void Awake()
     {
-        
+        inputActions = new InputSystem_Actions();
+
+
     }
 
     // Update is called once per frame
